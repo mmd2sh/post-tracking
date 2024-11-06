a2246e231b558b9f92b12c0cb1239817fc7a83e1
Mohammad Shabani <51379824+mmd2sh@users.noreply.github.com>
2024-11-06 17:06:36 +0330

    Update searchView.js

--- searchView.js
+++ searchView.js
@@ -8,15 +8,15 @@ if (!window.isTrackingModuleInited) { // FIX MODULE BUILDER RUN TWICE!!!
         $(document).ready(function() {
             const loading = $('.tracking-search-loading', mbWrap);
             
-            loading.each(function() {
-                const wrap = $(this).parent().find('[data-dbfield=trackingCode]').parent();
-                $(this).appendTo(wrap);
-            });
+            // loading.each(function() {
+            //     const wrap = $(this).parent().find('[data-dbfield=trackingCode]').parent();
+            //     $(this).appendTo(wrap);
+            // });
             
             const submitBtn = $('[id*=search_wrapper]:has(.tracking-search-wrap) input.drt-searchBtn[data-identifier*=search_]', mbWrap);
             
             submitBtn.each(function(_, btn) {
-                const wrap = $(btn).parent().find('[data-dbfield=trackingCode]').parent();
+                const wrap = $('.tracking-search-bar', mbWrap);
                 $(btn).appendTo(wrap);
                 
                 $(btn).on('click', function() {
@@ -32,11 +32,26 @@ if (!window.isTrackingModuleInited) { // FIX MODULE BUILDER RUN TWICE!!!
             
             input.prop('placeholder', 'شماره مرسوله را وارد کنید.');
             
-            // input.on('input', function() {
-            //     this.value = this.value.trim();
-            //     let isValid = this.value.length == 24;
-            //     submitBtn.prop('disabled', !isValid);
-            // });
+            
+            input.on('input paste change', function() {
+                // Convert Arabic and Persian digits to English
+                this.value = this.value.replace(/[۰-۹]/g, function(d) {
+                    return String.fromCharCode(d.charCodeAt(0) - 1728);
+                }).replace(/[٠-٩]/g, function(d) {
+                    return String.fromCharCode(d.charCodeAt(0) - 1584);
+                });
+            
+                // Remove non-digit characters and trim length to 24 characters
+                this.value = this.value.replace(/\D/g, '').substring(0, 24);
+            
+                let isValid = this.value.length == 24;
+            
+                // Update character counter
+                $('.tracking-search-counter bdi').text(this.value.length);
+            
+                // submitBtn.prop('disabled', !isValid);
+            });
+
             
             mbWrap.on('click', '.dp-alertBox', function() {
                 $(this).closest('[data-module-layout-wrapper]').hide();
@@ -49,6 +64,16 @@ if (!window.isTrackingModuleInited) { // FIX MODULE BUILDER RUN TWICE!!!
                 if (ev.key === 'Enter' || ev.keyCode === 13) {
                     submitBtn.click();
                 }
+            });
+            
+            $('.tracking-search-paste', mbWrap).on('click', async function() {
+                try {
+                    const text = await navigator.clipboard.readText();
+                    input.val(text).change();
+                } catch (err) {
+                    console.error("Failed to read clipboard contents:", err);
+                }
+                
             });
 
         });
