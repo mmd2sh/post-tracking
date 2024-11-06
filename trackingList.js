09eb042dccf2519a7da1da36096a63b8f2fd8dae
Mohammad Shabani <51379824+mmd2sh@users.noreply.github.com>
2024-11-06 11:45:22 +0330

    Update and rename trackingList.html to trackingList.js

--- trackingList.js
+++ trackingList.js
@@ -1,4 +1,3 @@
-<script>
 !function() {
     const thisScript = document.currentScript;
     const mbWrap = $(thisScript).closest('.mb-wrapper');
@@ -198,4 +197,3 @@
         }[key] || key;
     }
 }();
-</script>
